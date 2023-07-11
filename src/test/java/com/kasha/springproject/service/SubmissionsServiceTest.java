6c33a702177d90ab3cce6edaa09e18099103b6c2
udaykumarkasha <udayrk19@gmail.com>
2023-07-11 15:56:56 -0500

    Unit test cases updated

--- src/test/java/com/kasha/springproject/service/SubmissionsServiceTest.java
+++ src/test/java/com/kasha/springproject/service/SubmissionsServiceTest.java
@@ -33,106 +33,82 @@ public class SubmissionsServiceTest {
 
     @Test
     public void testGetAllSubmissions() {
-        // Mock data
         List<SubmissionsDTO> mockSubmissions = Arrays.asList(
                 new SubmissionsDTO(1, "Submission 1"),
                 new SubmissionsDTO(2, "Submission 2"),
                 new SubmissionsDTO(3, "Submission 3")
         );
 
-        // Mock the repository behavior
         when(submissionRepository.getAllSubmissions()).thenReturn(mockSubmissions);
 
-        // Call the service method
         List<SubmissionsDTO> result = submissionsService.getAllSubmissions();
 
-        // Verify the result
         assertEquals(mockSubmissions, result);
     }
 
     @Test
     public void testGetAllSubmissionsEmpty() {
-        // Mock the repository behavior to return an empty list
         when(submissionRepository.getAllSubmissions()).thenReturn(Collections.emptyList());
 
-        // Call the service method
         List<SubmissionsDTO> result = submissionsService.getAllSubmissions();
 
-        // Verify the result is an empty list
         assertTrue(result.isEmpty());
     }
 
     @Test
     public void testCreateSubmission() {
-        // Mock data
         SubmissionsDTO mockSubmission = new SubmissionsDTO(1, "New Submission");
 
-        // Mock the repository behavior
         when(submissionRepository.createSubmission(any(SubmissionsDTO.class))).thenReturn(mockSubmission);
 
-        // Call the service method
         SubmissionsDTO result = submissionsService.createSubmission(mockSubmission);
 
-        // Verify the result
         assertEquals(mockSubmission, result);
     }
 
     @Test
     public void testCreateSubmissionNull() {
-        // Mock the repository behavior to return null when creating a submission
         when(submissionRepository.createSubmission(any(SubmissionsDTO.class))).thenReturn(null);
 
-        // Call the service method
         SubmissionsDTO result = submissionsService.createSubmission(null);
 
-        // Verify the result is null
         assertNull(result);
     }
 
     @Test
     public void testUpdateSubmission() {
-        // Mock data
         int submissionId = 1;
         SubmissionsDTO updatedSubmission = new SubmissionsDTO(submissionId, "Updated Submission");
 
-        // Mock the repository behavior
         when(submissionRepository.updateSubmission(eq(submissionId), any(SubmissionsDTO.class))).thenReturn(updatedSubmission);
 
-        // Call the service method
         SubmissionsDTO result = submissionsService.updateSubmission(submissionId, updatedSubmission);
 
-        // Verify the result
         assertEquals(updatedSubmission, result);
     }
 
     @Test
     public void testUpdateSubmissionNotFound() {
-        // Mock data
+
         int submissionId = 1;
         SubmissionsDTO updatedSubmission = new SubmissionsDTO(submissionId, "Updated Submission");
 
-        // Mock the repository behavior to return null when updating a non-existent submission
         when(submissionRepository.updateSubmission(eq(submissionId), any(SubmissionsDTO.class))).thenReturn(null);
 
-        // Call the service method
         SubmissionsDTO result = submissionsService.updateSubmission(submissionId, updatedSubmission);
 
-        // Verify the result is null
         assertNull(result);
     }
 
     @Test
     public void testDeleteSubmission() {
-        // Mock data
+
         int submissionId = 1;
 
-        // Call the service method
         boolean result = submissionsService.deleteSubmission(submissionId);
 
-        // Verify that the repository method was called
         verify(submissionRepository).deleteSubmission(submissionId);
 
-        // Verify the result is false (hard-coded in the original method)
         assertFalse(result);
     }
 
